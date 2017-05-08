dac84d9063d895a689f2a6bac19a6dcbd28935b5
Uraeus Ray Millet <uraeusmillet@gmail.com>
2017-05-08 23:36:54 +0000

    init mongodb in app

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const searches = null;
+const MONGODB_URI = process.env.MONGOLAB_URI || "mongodb://localhost:27017/test";
 
 app.get('/', function(req, res) {
     res.send("Hello World");
+})
+
+// get img url
+app.get('/api/imagesearch/:search', function(req, res) {
+    // get search from request object
+    const search = req.body.search;
+    
+    // save search in db
+    // response object contains search term and time term was queried?
+    res.json({
+        
+    })
+    
 })
 
 app.listen(8080, function() {
